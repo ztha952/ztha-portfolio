fa6a2d8d4df47fb85f6797ebd9e33dbbd2a07e6c
ztha952 <88443837+ztha952@users.noreply.github.com>
2021-12-12 14:10:48 +0630

    jpg to webp

--- pages/api/data.js
+++ pages/api/data.js
@@ -2,19 +2,19 @@ export const pjs = [
   {
     id: 1,
     name: "N&CO HOMEMADE(UX)",
-    img: "https://i.ibb.co/0GT4LFk/pj-3.png",
+    img: "https://i.ibb.co/jgwxQfy/pj-3.webp",
     link: "https://www.figma.com/file/RLujD6AYvRhvw7ikI8ltbr/Test-Design?node-id=101%3A2",
   },
   {
     id: 2,
     name: "Travelers(UX)",
-    img: "https://i.ibb.co/25Zqc9z/pj-2.png",
+    img: "https://i.ibb.co/LkWC03Q/pj-2.webp",
     link: "https://www.figma.com/file/NVwKVi78nWk8ZyuVWmoNHk/Untitled",
   },
   {
     id: 3,
     name: "ZCREAM STORE(UX)",
-    img: " https://i.ibb.co/6PLB5Gh/pj-1.png",
+    img: "https://i.ibb.co/y5yb5h5/pj-1.webp",
     link: "https://www.figma.com/file/NVwKVi78nWk8ZyuVWmoNHk/Untitled",
   },
   {
@@ -38,19 +38,19 @@ export const pjs = [
   {
     id: 4,
     name: " ZORGANIC(UX)",
-    img: " https://i.ibb.co/f1xHG8Z/pj-4.png",
+    img: " https://i.ibb.co/wsJtqTC/pj-4.webp",
     link: "https://www.figma.com/file/9QV4XMiQFLJ5PQ9C2ydle6/Untitled",
   },
   {
     id: 5,
     name: "Ujin Minimalist Store(UX)",
-    img: " https://i.ibb.co/BcG6hrF/pj-6.png",
+    img: " https://i.ibb.co/nLdrVb5/pj-6.webp",
     link: "https://www.figma.com/file/4378x13XlPuCs3NX6DBhWG/Untitled?node-id=0%3A1",
   },
   {
     id: 6,
     name: "HELO FASHION (UX)",
-    img: "https://i.ibb.co/41nMDB5/pj-5.png",
+    img: "https://i.ibb.co/jH3BXtq/pj-5.webp",
     link: "https://www.figma.com/file/gPv20u4KcVTAelSV8cs9qn/Untitled?node-id=0%3A1",
   },
 ];
